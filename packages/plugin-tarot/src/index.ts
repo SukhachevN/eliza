91901b146f4658c70924979a0b4d5ce40ca06f13
Nikita Sukhachev <traed.traed@mail.ru>
2025-02-21 17:47:51 +0700

    feat: spread tarot only for twitter posts

--- packages/plugin-tarot/src/index.ts
+++ packages/plugin-tarot/src/index.ts
@@ -1,13 +1,13 @@
 import { Plugin } from "@elizaos/core";
 import { tokensProvider } from "./providers";
-import { generate, getTarotPrediction } from "./actions";
+import { getTarotPrediction } from "./actions";
 
 export { getTarotPrediction };
 
 export const tarotPlugin: Plugin = {
     name: "tarot",
     description: "Tarot plugin",
-    actions: [generate],
+    actions: [],
     evaluators: [],
     providers: [tokensProvider],
 };
