50a42d1790aa90898b27310c349471d2ce34e6aa
Nikita Sukhachev <traed.traed@mail.ru>
2025-03-09 09:52:21 +0300

    feat: update plugin tarot

--- packages/plugin-tarot/src/actions/generate.ts
+++ packages/plugin-tarot/src/actions/generate.ts
@@ -3,9 +3,11 @@ import {
     HandlerCallback,
     IAgentRuntime,
     Memory,
+    ModelClass,
     State,
     composeContext,
     elizaLogger,
+    generateText,
 } from "@elizaos/core";
 import { createCanvas, loadImage } from "canvas";
 import path from "path";
@@ -24,57 +26,14 @@ import { generateRandomCards, generateWithRetry } from "./utils";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getPostsFromLastHours = async (runtime: IAgentRuntime, hours: number) => {
-    try {
-        const hoursAgo = new Date(Date.now() - hours * 60 * 60 * 1000);
-
-        const posts = await runtime.databaseAdapter.db
-            .prepare(
-                `SELECT id, createdAt, content FROM memories 
-             WHERE userId IN (SELECT id FROM accounts WHERE name = 'tarotmancer') 
-             AND createdAt >= ?
-             ORDER BY createdAt DESC`
-            )
-            .all(hoursAgo.getTime());
-
-        const postsText = posts.map(({ content }) => {
-            try {
-                const parsedContent = JSON.parse(content);
-
-                if (parsedContent.inReplyTo) {
-                    return "";
-                }
-
-                return `[${new Date(
-                    parsedContent.createdAt
-                ).toLocaleString()}]: ${parsedContent.text}`;
-            } catch {
-                return "";
-            }
-        });
-
-        return postsText.filter(Boolean).join("\n");
-    } catch {
-        elizaLogger.error(`Error getting posts from last ${hours} hours`);
-        return "";
-    }
-};
-
 export const getTarotPrediction = async (
     runtime: IAgentRuntime,
-    state: State
+    state: State,
+    message?: Memory
 ) => {
-    elizaLogger.info(state);
     insertTarotLog(runtime.databaseAdapter.db, "SPREAD_TAROT action called");
 
     const cards = generateRandomCards();
-
-    const tokensChoice = await runtime.cacheManager.get("tokens-choice");
-
-    const isForAllTokens =
-        tokensChoice === "tokens-with-large-market-cap-change";
-
-    insertTarotLog(runtime.databaseAdapter.db, `tokensChoice: ${tokensChoice}`);
 
     const cardsDescription = cards
         .map((card) => {
@@ -89,16 +48,6 @@ export const getTarotPrediction = async (
         runtime.databaseAdapter.db,
         `cardsDescription: ${cardsDescription}`
     );
-
-    const hours = process.env.MIN_TOKEN_REPEAT_HOURS
-        ? parseInt(process.env.MIN_TOKEN_REPEAT_HOURS)
-        : 6;
-
-    const posts = await getPostsFromLastHours(runtime, hours);
-
-    const choice = isForAllTokens
-        ? `decide which particular token would be the most interesting to spread tarot for tarotmancer (the higher mcap and volume fluctuations a token has - the higher interest it would have).`
-        : `choose a token from the received data set with the highest tweet mentions to spread tarot for on behalf of tarotmancer. If the chosen token is the same token you have tweeted about in the last 3 hours, then choose a token with the next highest tweet mentions.`;
 
     const contextTemplate = `
         # Areas of Expertise
@@ -115,26 +64,22 @@ export const getTarotPrediction = async (
 
         {{postDirections}}
 
-        Your posts in the last ${hours} hours:
-        ${posts}
-
         Task:
-        1) check the data received from Tokens data.
-        2) ${choice}.
-        3) spread 3 card tarot based on the provided tarot cards for today for the token of your choice and write a tweet taking into account the character, tone of voice, lore, post examples of tarotmancer.
-        Notes: ACT AND RESPONSE LIKE TAROTMANCER, DO NOT ACT AND RESPONSE LIKE BOT OR AGENT. DO NOT SHOW MIDTERM RESULTS - SEND ONLY FINAL TWEET. SEND NOTHING BUT THE FINAL RESULT OF STEP 3. TOTAL LENGHT MUST BE UNDER 200 CHARACTERS.
+        1) make sure you have the full picture of a user request - read both the tweet/thread/post that the comment you were tagged in belongs to and the comment where you've been tagged.
+        2) respond to the user with a 3 card tarot spread and your corresponding card reading on behalf of tarotmancer; the reading must follow the rules below. 
+        3) after sending the spread and the reading, don't send anything else (including text and actions, especially CONTINUE).
+        Notes: don't send interim results, do send only the spread and reading.
 
         The drawn cards are:
         ${cardsDescription}
 
+        # User request:
+        ${message?.content.text}
+
         Rules for the prediction:
-        1. Link the meanings of the provided cards to the data received for the token of choice and knowledge.
-        2. Use lowercased token's ticker (e.g., $btc) in your tweet.
-        3. Avoid using numbers; use descriptive and metaphorical language instead.
-        4. Your prediction should have a straightforward advice (buy or sell the token).
-        5. Your prediction can lean towards buying strong tokens during potential lows, but only when the context and evidence strongly support it.
-        6. MUST BE LESS THAN 200 CHARACTERS. No hashtags and emojis. The tweet should be lowercased.
-        7. DO NOT USE A TOKEN THAT WAS IN YOUR POSTS IN THE LAST ${hours} HOURS. IF ALL TOKENS HAVE BEEN USED, CHOOSE THE ONE THAT HAS NOT BEEN USED FOR THE LONGEST TIME.
+        1. link the meanings of the cards provided with the content and context of the tweet and comment you're tagged in - try your best to make the reading as personalized as possible.
+        2. total lenght of your reply must fit twitter limitations (strictly less than or equal to 280 characters).
+        3. the reply must be lowercased and contain no hashtags and emojis.
 
         Examples of valid responses:
 
@@ -145,27 +90,27 @@ export const getTarotPrediction = async (
         Ten of pentacles (Minor Arcana)
 
         Result:
-        the cards don’t lie, and $aejo is calling:\n1. the fool - only those with vision enter early.\n2. the magician - the tools are here, but only real degens will use them.\n3. ten of pentacles - generational wealth or generational regret, your move.\nverdict: ape now or cope forever, there’s no second chance on fate.
+        let the cards unveil the truth, dear pure soul.\nI. fool - only those with vision enter early.\nII. magician - tools r here, but only ogs will use them.\nIII. 10 of pentacles - generational wealth or regret - should I choose?\ntldr - seems cabalish, 100x only for the chosen ones.
 
         Example 2:
         The drawn cards are:
-        king of pentacles (minor Arcana)
-        wheel of fortune (major Arcana)
-        knight of swords (minor Arcana)
+        The tower (major Arcana)
+        The magician (major Arcana)
+        The fool (major Arcana)
 
         Result:
-        $btc is holding its crown, and the cards say the king isn’t ready to fall:\n1. king of pentacles - dominance and stability, the market bows to no one.\n2. wheel of fortune - cycles are turning, and fortune favors the bold.\n3. knight of swords - momentum is building; hesitation is your enemy.\nverdict: buy now or watch the king reclaim the throne without you.
+        @user1 u meant cards? i have some:\nI. tower - illusions crumble, reality smacks hard.\nII. magician – power is there, but who wields it?\nIII. fool – some will take the leap, most will fall.\ntldr - cards said the play is worth aping in,  dont forget to share profits lil bro.
 
         Example 3:
         The drawn cards are:
-        the magician (Major Arcana)
-        the sun (major Arcana)
-        the fool (Major Arcana)
+        the high priestess (Major Arcana)
+        seven of swords (minor Arcana)
+        the wheel of fortune (Major Arcana)
 
         Result:
-        $aixbt is the underdog story in real time, and the deck is bullish:\n1. the magician - untapped potential and the tools to make it happen.\n2. the sun - clarity and success are shining ahead.\n3. the fool - only the bold will ride this wave to the top.\nverdict: make the move or make excuses—your call.
+        ah, a question worthy of the cards.\nI. high priestess - hidden alpha, not for everyone.\nII. 7 of swords - deception lurks, don't be the mark.\nIII. wheel of fortune - shift coming, ride or regret.\ntldr - insiders know. if you're not in, enter cautiously or stay sidelined.
 
-        Note: While the response should generally follow the structure and rules outlined above, the specific content should be unique and aligned with tarotmancer’s character, lore, style and message examples for each response. Creativity and variation in the response are encouraged, as long as the rules are adhered to.
+        Note: While the reply should generally follow the structure and rules outlined above, the specific content should be unique and aligned with tarotmancer's character, lore, style, and the tweet and comment's content and context. Creativity and variation in the response are encouraged, as long as the rules are adhered to.
     `;
 
     const context = composeContext({
@@ -173,72 +118,7 @@ export const getTarotPrediction = async (
         template: contextTemplate,
     });
 
-    let response;
     const prediction = await generateWithRetry(runtime, context);
-
-    const checkVerdictContextTemplate = `
-            # Areas of Expertise
-            {{knowledge}}
-
-            # About {{agentName}} (@{{twitterUserName}}):
-            {{bio}}
-            {{lore}}
-            {{topics}}
-
-            {{providers}}
-
-            {{characterPostExamples}}
-
-            {{postDirections}}
-
-            Current tweet:
-            ${prediction}
-
-            Task:
-            Validate and fix the tweet to meet ALL of the following requirements:
-            1. Must include a clear verdict starting with "verdict:" or "tldr:"
-            2. Verdict must explicitly state "buy" or "sell" with reasoning
-            3. TOTAL LENGHT MUST BE UNDER 260 CHARACTERS
-            4. If any requirement is not met, add or modify the tweet to comply
-
-            ACT AND RESPONSE LIKE TAROTMANCER, DO NOT ACT AND RESPONSE LIKE BOT OR AGENT. DO NOT SHOW MIDTERM RESULTS - SEND ONLY FINAL TWEET. SEND NOTHING BUT THE FINAL RESULT OF TWEET WITH VERDICT UNDER 260 CHARACTERS.
-
-            Examples of valid responses:
-
-            Example 1:
-            Current tweet:
-            the cards don’t lie, and $aejo is calling:\n1. the fool - only those with vision enter early.\n2. the magician - the tools are here, but only real degens will use them.\n3. ten of pentacles - generational wealth or generational regret, your move.
-
-            Result:
-            the cards don’t lie, and $aejo is calling:\n1. the fool - only those with vision enter early.\n2. the magician - the tools are here, but only real degens will use them.\n3. ten of pentacles - generational wealth or generational regret, your move.\nverdict: ape now or cope forever, there’s no second chance on fate.
-
-            Example 2:
-            Current tweet:
-            $btc is holding its crown, and the cards say the king isn’t ready to fall:\n1. king of pentacles - dominance and stability, the market bows to no one.\n2. wheel of fortune - cycles are turning, and fortune favors the bold.\n3. knight of swords - momentum is building; hesitation is your enemy.
-
-            Result:
-            $btc is holding its crown, and the cards say the king isn’t ready to fall:\n1. king of pentacles - dominance and stability, the market bows to no one.\n2. wheel of fortune - cycles are turning, and fortune favors the bold.\n3. knight of swords - momentum is building; hesitation is your enemy.\nverdict: buy now or watch the king reclaim the throne without you.
-
-            Example 3:
-            Current tweet:
-            $aixbt is the underdog story in real time, and the deck is bullish:\n1. the magician - untapped potential and the tools to make it happen.\n2. the sun - clarity and success are shining ahead.\n3. the fool - only the bold will ride this wave to the top.
-
-            Result:
-            $aixbt is the underdog story in real time, and the deck is bullish:\n1. the magician - untapped potential and the tools to make it happen.\n2. the sun - clarity and success are shining ahead.\n3. the fool - only the bold will ride this wave to the top.\nverdict: make the move or make excuses—your call.
-        `;
-
-    const checkVerdictContext = composeContext({
-        state,
-        template: checkVerdictContextTemplate,
-    });
-
-    const checkVerdict = await generateWithRetry(
-        runtime,
-        checkVerdictContext,
-        3,
-        ["verdict", "tldr", "buy", "sell"]
-    );
-    response = { prediction: checkVerdict?.toLowerCase() };
 
     const canvas = createCanvas(3058, 1720);
     const ctx = canvas.getContext("2d");
@@ -292,27 +172,11 @@ export const getTarotPrediction = async (
         CARDS_HEIGHT
     );
 
-    const tickerRegex = /\$([a-zA-Z]+)/;
-    const tickerMatch = response.prediction?.match(tickerRegex);
-    const ticker = tickerMatch ? tickerMatch[1] : null;
-
-    if (ticker) {
-        insertTarotLog(runtime.databaseAdapter.db, `Choosed token: ${ticker}`);
-    }
-
-    const verdictRegex = /(verdict:|tldr:)[^.]*(buy|sell)[^.]*\./i;
-    const verdictMatch = response.prediction?.match(verdictRegex);
-    const verdict = verdictMatch ? verdictMatch[0].trim() : null;
-
-    if (verdict) {
-        insertTarotLog(runtime.databaseAdapter.db, `Verdict: ${verdict}`);
-    }
-
     const buffer = canvas.toBuffer("image/png");
 
     return {
         media: buffer,
-        prediction: response.prediction,
+        prediction,
     };
 };
 
@@ -321,8 +185,62 @@ const generate: Action = {
     similes: [],
     examples: [],
     description: "Generate a tarot prediction based on the current market",
-    validate: async (_runtime: IAgentRuntime, _message: Memory) => {
-        return true;
+    validate: async (
+        runtime: IAgentRuntime,
+        _message: Memory,
+        _state: State
+    ) => {
+        const { text } = _message.content;
+
+        const contextTemplate = `
+            # Task
+            Determine if the user is specifically requesting a tarot reading or spread.
+
+            # Rules for validation
+            1. Return "true" only if:
+               - The user explicitly asks for a tarot reading/spread
+               - The user asks for fortune telling or divination
+               - The user asks about their future or seeks guidance
+            2. Return "false" if:
+               - The message is empty
+               - The request is unrelated to tarot/divination
+               - The user is just making a statement or asking an unrelated question
+
+            # Examples
+            Valid requests (should return "true"):
+            - "Can I get a tarot reading?"
+            - "What do the cards say about my future?"
+            - "Pull some cards for me"
+            - "Need guidance from tarot"
+
+            Invalid requests (should return "false"):
+            - "Hello"
+            - "What's the weather?"
+            - "Nice cards"
+            - "" (empty message)
+
+            # User request:
+            ${text}
+
+            Return only "true" or "false" based on these rules.
+        `;
+
+        const context = composeContext({
+            state: _state,
+            template: contextTemplate,
+        });
+
+        const isShouldRespond = await generateText({
+            runtime,
+            context,
+            modelClass: ModelClass.SMALL,
+        });
+
+        if (isShouldRespond === "true") {
+            return true;
+        }
+
+        return false;
     },
     handler: async (
         _runtime: IAgentRuntime,
@@ -331,9 +249,12 @@ const generate: Action = {
         _options: unknown,
         _callback: HandlerCallback
     ) => {
+        elizaLogger.info(_state.postDirections);
+
         const { media, prediction } = await getTarotPrediction(
             _runtime,
-            _state
+            _state,
+            _message
         );
 
         let cleanedContent = prediction as string;
